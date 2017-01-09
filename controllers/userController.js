6933afc21f40e6fc4944fb80790dfaa3ab8eece1
Colin <colin@MacBook-Pro-de-Colin.local>
2017-01-09 14:44:44 +0100

    +error message

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,22 +10,22 @@ module.exports.register = function(req, res) {
 
   console.log(req.body);
 
-  var lastName     = req.body.lastname;
+  var lastName = req.body.lastname;
   var firstName = req.body.firstname;
   var password = req.body.password;
   var email = req.body.email;
   //var country = req.body.country;
-
+// INSCRIPTION DEBUT
   User.create({
     LastName : lastName,
     FirstName     : firstName,
-    email: email,
+    Email: email,
     //country: country,
-    password : bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    Password : bcrypt.hashSync(password, bcrypt.genSaltSync(10))
   }, function(err, user) {
     if(err) {
       console.log(err);
-      res.status(404).render('error');
+      res.status(500).render('inscription', {error : 'Email deja utilisé, veuillez en choisir une autre !'});
     } else {
       console.log('user created', user);
       res.status(201).render('reservation');
--- db/models/users.model.js
+++ db/models/users.model.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var User = new mongoose.Schema({
   FirstName : String,
   LastName : String,
-  Email : String,
+  Email : { type: String, index:{unique: true}},
   Password : String
 });
-mongoose.model("User", User);
+mongoose.model("User", User, "users");
