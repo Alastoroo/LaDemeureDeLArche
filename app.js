2b9beb4c74f4706b2f23304143c33e2c4c42b7c2
bonneau <boubagraph@gmail.com>
2017-01-10 15:04:30 +0100

    modif navbar + tourisme

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/home', routes);
+app.use('/demeure', routes);
 app.use('/sejours', routes);
+app.use('/tourisme', routes);
+app.use('/livre', routes);
+app.use('/reservation', routes);
+app.use('/contact', routes);
 
 var server = app.listen(app.get("port"), function(){
     console.log("Le serveur tourne sur "+server.address().address+":"+server.address().port)
--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,37 @@ var jade = require('jade');
        .get(function(req,res){
          res.render("index");
        });
+
+       router
+         .route("/demeure")
+         .get(function(req,res){
+           res.render("demeure");
+         });
        router
          .route("/sejours")
          .get(function(req,res){
            res.render("sejours");
          });
+         router
+           .route("/tourisme")
+           .get(function(req,res){
+             res.render("tourisme");
+           });
+           router
+             .route("/livre")
+             .get(function(req,res){
+               res.render("livre");
+             });
+            router
+               .route("/contact")
+               .get(function(req,res){
+                 res.render("contact");
+               });
+               router
+                  .route("/reservation")
+                  .get(function(req,res){
+                    res.render("reservation");
+                  });
 
        router
              .route('/users') // on declare la route
