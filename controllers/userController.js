c8c3671fce42c9d5dfa853eb797ab5418cc2d010
Colin <colin@MacBook-Pro-de-Colin.local>
2017-01-11 18:13:18 +0100

    modif du back

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var User     = mongoose.model('User');
 var bcrypt   = require('bcrypt-nodejs');
 //var jwt      = require('jsonwebtoken');
@@ -16,8 +17,10 @@ module.exports.register = function(req, res) {
   var password = req.body.password;
   var email = req.body.email;
   //var country = req.body.country;
+
 // INSCRIPTION DEBUT
-  User.create({
+  User
+    .create({
     LastName : lastName,
     FirstName : firstName,
     Email: email,
@@ -33,4 +36,22 @@ module.exports.register = function(req, res) {
     }
   });
 };
+// CONNEXION DEBUT
+  module.exports.login = function(req, res){
+    passport.use(new LocalStrategy(
+      function(req.body.email, req.body.password, done ){
+        User
+        .find({email: req.body.email})
+        .exec(function(err , user){
+          if(err){
+            done(err);
+          }else if(req.body.password != user.password){
+            done(null, false);
+          } else {
+            done(null, user);
+          }
+        })
+      }
+    ))
+  }
 // MODULE FIN
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var passport = require('passport');
 var express = require("express");
 var router = express.Router();
 var path = require('path');
@@ -5,20 +6,20 @@ var jade = require('jade');
 
 var ctrlUser = require('../controllers/userController.js');
 
+//ROUTE PERMET D'AFFICHER LE CONTENUE
+           router
+            .route("/")
+            .get(function(req,res){
+             res.render("index");
+           });
 
-     router
-       .route("/")
-       .get(function(req,res){
-         res.render("index");
-       });
+           router
+            .route("/sejours")
+            .get(function(req,res){
+             res.render("sejours");
+           });
 
-       router
-         .route("/sejours")
-         .get(function(req,res){
-           res.render("sejours");
-         });
-
-         router
+           router
            .route("/demeure")
            .get(function(req,res){
              res.render("demeure");
@@ -57,39 +58,14 @@ var ctrlUser = require('../controllers/userController.js');
 
             router
             .route("/connexion")
-            //.post(ctrlUser.register)
+            .post(ctrlUser.login)
             .get(function(req, res){
                 res.render("connexion");
             });
 
 
 
-      //  router
-      //        .route('/users') // on declare la route
-       //
-      //        .get(function(req,res){ //on va recuperer les users
-      //          User
-      //              .find() // on va aller chercher tout les utilisateur dans notre base donné
-      //              .exec(function(err, users){
-      //                if(err) {
-      //                  res.status(500);
-      //                  res.render("error", err);// on defini la page que l'on veut afficher
-      //                } else {
-      //                  res.render("users-list", {users: users}); // prend la donné users pour l'afficher
-      //                }
-      //              });
-      //        });
 
-// CONEXION
-      // router
-      // .route("/connexion")
-      // app.post('/connexion',
-      // passport.authenticate('local', { successRedirect: '/reservation', successFlash: 'Bienvenue!', failureRedirect: '/connexion', failureFlash: 'Email ou mot de passe invalide, veuillez reesayer'}),
-      // function(req, res) {
-      //   // If this function gets called, authentication was successful.
-      //   // `req.user` contains the authenticated user.
-      //   res.redirect('/users/' + req.user.username); // ??????? METRE req.body.lastName ??????????
-      // });
 
 
 
