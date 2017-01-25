44bc486b01abebdc4e269f6c7937a18952b0ed91
Alastoroo <colinblechet@yahoo.fr>
2017-01-25 20:13:07 +0100

    modif

--- routes/index.js
+++ routes/index.js
@@ -65,7 +65,7 @@ module.exports = function (app, passport){
         console.log(err);
       } else {
 
-        description = {description : homeDescription[homeDescription.length-1].presentation !== undefined ? homeDescription[homeDescription.length-1].presentation : null};
+        description = {description : homeDescription[homeDescription.length-1].presentation !== "undefined" ? homeDescription[homeDescription.length-1].presentation : null};
 
         res.render("admin/home", description);
 
