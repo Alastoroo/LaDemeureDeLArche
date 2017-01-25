0629218c1f433d2a639df069ba7465c2c2ee6b89
mat28 <mathieu@rubixconsult.com>
2017-01-25 20:04:52 +0100

    caca

--- routes/index.js
+++ routes/index.js
@@ -64,8 +64,8 @@ module.exports = function (app, passport){
       if(err){
         console.log(err);
       } else {
-        console.log(homeDescription[homeDescription.length-1].presentation);
-        res.render("admin/home", {description : homeDescription[homeDescription.length-1].presentation});
+        description = {description : homeDescription[homeDescription.length-1].presentation ? homeDescription[homeDescription.length-1].presentation : null};
+        res.render("admin/home", description);
       }
     });
   });
