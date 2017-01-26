49f24f79d4c5b502ab1da3cdeb18dc6f38248e00
mat28 <mathieu@rubixconsult.com>
2017-01-26 14:27:46 +0100

    caca

--- db/models/demeure.model.js
+++ db/models/demeure.model.js
@@ -7,7 +7,12 @@ var demeureDescriptionSchema = new mongoose.Schema({
 var demeureImageSchema = new mongoose.Schema({
   url : String
 });
+var demeureEquipementSchema = new mongoose.Schema({
+  title : String,
+  description : String,
+  urlImage : String
+});
 
-
+mongoose.model("DemeureEquipement", demeureEquipementSchema,'demeureEquipement');
 mongoose.model("DemeureDescription", demeureDescriptionSchema, "demeureDescription");
 mongoose.model("DemeureImage", demeureImageSchema, "demeureImage");
